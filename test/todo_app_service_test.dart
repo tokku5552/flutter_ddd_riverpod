2dc7b12cdafd8e0fc4e90d8925345c7aa7a0459b
tokku5552 <physics.0121@gmail.com>
2022-01-27 23:25:08 +0900

    implement unit test

--- test/todo_app_service_test.dart
+++ test/todo_app_service_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_ddd_riverpod/application/todo_app_service.dart';
 import 'package:flutter_ddd_riverpod/domain/todo_item.dart';
 import 'package:flutter_ddd_riverpod/domain/value/todo_id.dart';
@@ -19,35 +20,84 @@ void main() {
     _repository.reset();
   });
 
-  test('findByTodoId', () async {
-    final item = TodoItem.initial();
-    await _repository.create(item: item);
-    final nextId = _repository.nextId;
+  group('findByTodoId', () {
+    test('TodoId(\'0000000000\')', () async {
+      _repository.set(dummyTodoList);
 
-    final service = _container.read(todoAppService);
-    final result = await service.findByTodoId(todoId: TodoId(nextId));
-    expect(
-      result,
-      item.copyWith(
-        id: TodoId(nextId),
+      final service = _container.read(todoAppService);
+      final result = await service.findByTodoId(
+        todoId: const TodoId('0000000000'),
+      );
+      expect(result, dummyTodoList[0]);
+    });
+  });
+
+  group('updateIsDone', () {
+    test('updateIsDone', () async {
+      _repository.set(dummyTodoList);
+      final service = _container.read(todoAppService);
+      final result1 =
+          await service.findByTodoId(todoId: TodoId(dummyTodoList[0].id.value));
+      expect(result1, dummyTodoList[0]);
+
+      await service.updateIsDone(todoId: dummyTodoList[0].id);
+      final result2 =
+          await service.findByTodoId(todoId: TodoId(dummyTodoList[0].id.value));
+      expect(result2.isDone, !dummyTodoList[0].isDone);
+    });
+  });
+
+  group('updateTodoItem', () {
+    test('update item', () async {
+      _repository.set(dummyTodoList);
+      final service = _container.read(todoAppService);
+
+      final item = TodoItem.initial();
+      final updatedItem = item.copyWith(
+        id: dummyTodoList[0].id,
         title: item.title,
         detail: item.detail,
         isDone: item.isDone,
         createdAt: item.createdAt,
-      ),
-    );
+      );
+
+      await service.updateTodoItem(item: updatedItem);
+      final result =
+          await service.findByTodoId(todoId: TodoId(dummyTodoList[0].id.value));
+      expect(result, updatedItem);
+    });
   });
 
-  test('updateIsDone', () async {
-    _repository.set(dummyTodoList);
-    final service = _container.read(todoAppService);
-    final result1 =
-        await service.findByTodoId(todoId: TodoId(dummyTodoList[0].id.value));
-    expect(result1, dummyTodoList[0]);
+  group('deleteTodoItem', () {
+    test('delete item', () async {
+      _repository.set(dummyTodoList);
+      final service = _container.read(todoAppService);
 
-    await service.updateIsDone(todoId: dummyTodoList[0].id);
-    final result2 =
-        await service.findByTodoId(todoId: TodoId(dummyTodoList[0].id.value));
-    expect(result2.isDone, !dummyTodoList[0].isDone);
+      await service.deleteTodoItem(todoId: TodoId(dummyTodoList[0].id.value));
+      final result = _repository.get();
+
+      expect(result.containsKey(dummyTodoList[0].id.value), false);
+    });
+  });
+
+  group('createTodoItem', () {
+    test('create item', () async {
+      final item = TodoItem.initial();
+      await _repository.create(item: item);
+      final nextId = _repository.nextId;
+
+      final service = _container.read(todoAppService);
+      final result = await service.findByTodoId(todoId: TodoId(nextId));
+      expect(
+        result,
+        item.copyWith(
+          id: TodoId(nextId),
+          title: item.title,
+          detail: item.detail,
+          isDone: item.isDone,
+          createdAt: item.createdAt,
+        ),
+      );
+    });
   });
 }
